5776c49637d7e0de00247f4db9efee72f4436ad7
cwhetstone <cwhetstone@betterworldbooks.com>
2019-04-11 15:39:40 -0400

    Here we go

--- Program.cs
+++ Program.cs
@@ -9,104 +9,125 @@ namespace dogs
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! Welcome to the dog age calculator! To start, tell us how old you are!");
-            string myAge = Console.ReadLine();
+            string myAge = Console.ReadLine().ToLower();
             int age;
             int dogYears = 7;
-            bool rightAnswer=false;
+            bool rightAnswer = false;
+
+
+
 
             //Checks for Int. If not it repeats until it's an int
             if (!int.TryParse(myAge, out age))
             {
                 Console.WriteLine("{0} is not an integer", myAge);
                 rightAnswer = false;
-               while (rightAnswer == false){
+                while (rightAnswer == false)
+                {
                     Console.WriteLine("Please enter your age as an integer!");
-                    myAge = Console.ReadLine();
-                    if(int.TryParse(myAge, out age)){
+                    myAge = Console.ReadLine().ToLower();
+                    if (int.TryParse(myAge, out age))
+                    {
                         rightAnswer = true;
                         continue;
                     }
 
                 }
             }
-            else{
+            else
+            {
                 rightAnswer = true;
             }
+
+
+
+
             //Once answer is an int
             if (rightAnswer == true)
             {
                 Console.WriteLine("Would you like to see your age in dog years? (yes/no)");
                 var myOtherDogAge = (age + 1.5) * dogYears;
                 var myDogAge = age * dogYears;
-             var answer = Console.ReadLine();
+                var answer = Console.ReadLine().ToLower();
 
-            
-                while (answer != "yes" && answer != "no"){
+
+                while (answer != "yes" && answer != "no" && answer != "yes/no")
+                {
                     Console.WriteLine($"{answer} is invalid. Would you like to see your age in dog years? (yes/no)");
-                    answer = Console.ReadLine();
+                    answer = Console.ReadLine().ToLower();
                     if (answer == "yes" || answer == "no")
                     {
                         break;
 
-                    } 
+                    }
+
                 }
 
                 //wants age in dog years
 
-                if (answer=="yes")
+                if (answer == "yes")
                 {
                     Console.WriteLine($"You are {myDogAge} in dog years!");
                     Console.WriteLine("Would you like to know how old in dog years you will be in 1.5 years? (yes/no)");
-                    string dogAnswer = Console.ReadLine();
+                    string dogAnswer = Console.ReadLine().ToLower();
                     ;
 
-                    while( dogAnswer!= "yes" && dogAnswer != "no"){
+                    while (dogAnswer != "yes" && dogAnswer != "no" && dogAnswer != "yes/no")
+                    {
                         Console.WriteLine($"{dogAnswer} is invalid. Would you like to know how old in dog years you will be in 1.5 years? (yes/no)");
-                        dogAnswer = Console.ReadLine();
+                        dogAnswer = Console.ReadLine().ToLower();
                         if (dogAnswer == "yes" || dogAnswer == "no")
                         {
                             break;
                         }
                     }
-                 
+
                     // wants age in dog years and additional years
                     if (dogAnswer == "yes")
                     {
-                     
+
                         Console.WriteLine($"In 1.5 years you will be {myOtherDogAge}");
                         Console.WriteLine("Would you like a high 5? (yes/no)");
-                        string highFive = Console.ReadLine();
-                       
-                        while (highFive!= "yes" && highFive != "no"){
+                        string highFive = Console.ReadLine().ToLower();
+
+                        while (highFive != "yes" && highFive != "no" && highFive != "yes/no")
+                        {
                             Console.WriteLine($"{highFive} is invalid. Would you like a high 5? (yes/no)");
-                            highFive = Console.ReadLine();
+                            highFive = Console.ReadLine().ToLower();
                             if (highFive == "yes" || highFive == "no")
                             {
                                 break;
                             }
                         }
-                       
+
                         // yes to both and then high five question
                         if (highFive == "yes")
                         {
-                          
                             Console.WriteLine("*I give you a high 5*");
                         }
-                        else
+                        else if (highFive == "no")
                         {
-                          
                             Console.WriteLine("*I slap you in the face*");
                         }
+
+                        else if (highFive == "yes/no")
+                        {
+                            Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                            System.Environment.Exit(1);
+
+                        }
+
                     }
                     //yes to first and no to second now high five question
-                    else if (dogAnswer== "no")
+                    else if (dogAnswer == "no")
                     {
 
                         Console.WriteLine("Would you like a high 5? (yes/no)");
-                        string highFive = Console.ReadLine();
-                        while (highFive != "yes"  && highFive != "no"){
+                        string highFive = Console.ReadLine().ToLower();
+                        while (highFive != "yes" && highFive != "no" && highFive != "yes/no")
+                        {
                             Console.WriteLine($"{highFive} is invalid. Would you like a high 5? (yes/no)");
-                            highFive = Console.ReadLine();
+                            highFive = Console.ReadLine().ToLower();
                             if (highFive == "yes" || highFive == "no")
                             {
                                 break;
@@ -116,22 +137,41 @@ namespace dogs
                         {
                             Console.WriteLine("*I give you a high 5*");
                         }
-                        else
+                        else if (highFive == "no")
                         {
                             Console.WriteLine("*I slap you in the face*");
                         }
+
+                        else if (highFive == "yes/no")
+                        {
+                            Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                            System.Environment.Exit(1);
+
+                        }
+
                     }
+
+                    else if (dogAnswer == "yes/no")
+                    {
+                        Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                        System.Environment.Exit(1);
+                    }
+
+
+
+
                 }
                 //doesn't want age in dog years now ask about additional age
-                else if (answer== "no")
+                else if (answer == "no")
                 {
 
                     Console.WriteLine("Ok would you be interested in knowing how old you would be in dog years in 1.5 years? (yes/no)");
-                    string dogAnswer = Console.ReadLine();
+                    string dogAnswer = Console.ReadLine().ToLower();
 
-                    while(dogAnswer != "yes" && dogAnswer!= "no") {
+                    while (dogAnswer != "yes" && dogAnswer != "no" && dogAnswer != "yes/no")
+                    {
                         Console.WriteLine($"{dogAnswer} is invalid. would you be interested in knowing how old you would be in dog years in 1.5 years? (yes/no)");
-                        dogAnswer = Console.ReadLine();
+                        dogAnswer = Console.ReadLine().ToLower();
                         if (dogAnswer == "yes" || dogAnswer == "no")
                         {
                             break;
@@ -142,63 +182,93 @@ namespace dogs
                     {
                         Console.WriteLine($"In 1.5 years you will be {myOtherDogAge}");
                         Console.WriteLine("Would you like a high 5? (yes/no)");
-                        string highFive = Console.ReadLine();
+                        string highFive = Console.ReadLine().ToLower();
 
-                        while (highFive != "yes" && highFive != "no"){
+                        while (highFive != "yes" && highFive != "no" && highFive != "yes/no")
+                        {
                             Console.WriteLine($"{highFive} is invalid. Would you like a high 5? (yes/no)");
-                            highFive = Console.ReadLine();
+                            highFive = Console.ReadLine().ToLower();
                             if (highFive == "yes" || highFive == "no")
                             {
                                 break;
                             }
                         }
-                     
+
 
                         if (highFive == "yes")
                         {
                             Console.WriteLine("*I give you a high 5*");
                         }
-                        else
+                        else if (highFive == "no")
                         {
                             Console.WriteLine("*I slap you in the face*");
                         }
+
+                        else if (highFive == "yes/no")
+                        {
+                            Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                            System.Environment.Exit(1);
+
+                        }
+
                     }
-                    else
+
+
+
+                    else if (dogAnswer == "no")
                     {
                         Console.WriteLine("Would you like a high 5? (yes/no)");
-                        string highFive = Console.ReadLine();
+                        string highFive = Console.ReadLine().ToLower();
 
-                        while (highFive != "yes" && highFive != "no")
+                        while (highFive != "yes" && highFive != "no" && highFive != "yes/no")
                         {
-                        Console.WriteLine($"{highFive} is invalid. Would you like a high 5 (yes/no)");
-                            highFive = Console.ReadLine();
+                            Console.WriteLine($"{highFive} is invalid. Would you like a high 5 (yes/no)");
+                            highFive = Console.ReadLine().ToLower();
                             if (highFive == "yes" && highFive == "no")
                             {
                                 break;
                             }
                         }
-         
+
 
                         if (highFive == "yes")
                         {
                             Console.WriteLine("*I give you a high 5*");
                         }
-                        else
+                        else if (highFive == "no")
                         {
                             Console.WriteLine("*I slap you in the face*");
                         }
+
+                        else if (highFive == "yes/no")
+                        {
+                            Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                            System.Environment.Exit(1);
+
+                        }
+
+
+
                     }
-
+                    else if (dogAnswer == "yes/no"){
+                        Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                        System.Environment.Exit(1);
+                    }
                 }
+                    else if (answer == "yes/no")
+                    {
+                        Console.WriteLine("You are an idiot and can not be trusted with this knowledge!");
+                        System.Environment.Exit(1);
+                    }
+                }
+
+
+
+                /*Console.WriteLine($"You are {age} old in human years!");
+                           Console.WriteLine($"That makes you {myDogAge} in dog years!");
+
+                Console.WriteLine($"If you are curious how old you will be in dog years in 1.5 years, you will be {myOtherDogAge}!"); */
+
             }
-
-
-
-            /*Console.WriteLine($"You are {age} old in human years!");
-                       Console.WriteLine($"That makes you {myDogAge} in dog years!");
-
-            Console.WriteLine($"If you are curious how old you will be in dog years in 1.5 years, you will be {myOtherDogAge}!"); */
-
         }
     }
-}
